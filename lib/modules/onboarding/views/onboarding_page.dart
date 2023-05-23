6ff319cf2aee0738eb1a1b4f88f479bd222bcd1e
Haresh Gediya <gediyaharesh@gmail.com>
2023-05-23 09:49:18 +0530

    OnboardingPage UI update

--- lib/modules/onboarding/views/onboarding_page.dart
+++ lib/modules/onboarding/views/onboarding_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:laundry_app/core/app_colors.dart';
 import 'package:laundry_app/modules/authentication/authentication.dart';
-import 'package:laundry_app/widgets/app_buttons.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
@@ -9,57 +8,69 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        const Image(
-            height: 300, width: 395, image: AssetImage('assets/images/01.jpg')),
-        Container(
-          height: 550.9,
-          width: 400,
-          color: Color(0xFF34ABEF),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Row(
-                children: [
-                  SizedBox(width: 30),
-                  Text(
-                    'The best solution \nfor cleaning your \nclothes',
-                    style: TextStyle(fontSize: 35, color: Colors.white),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Row(
-                children: [
-                  SizedBox(width: 30),
-                  Text(
-                    'Find the best and closest laundery place to \nyou,cleaned with the best washing \nmachine so it doesn`t reduce the quality of \nyour clothes ',
-                    style: TextStyle(fontSize: 15, color: Colors.white),
-                  )
-                ],
-              ),
-              const SizedBox(height: 200),
-              SizedBox(
-                  height: 60,
-                  width: 330,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).pushReplacement(LoginPage.route());
-                    },
-                    child: Text(
-                      'Get started',
-                      style: TextStyle(color: AppColors.blue, fontSize: 17),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.white,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  )),
-            ],
+      backgroundColor: const Color(0xFF34ABEF),
+      body: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset(
+              'assets/images/01.jpg',
+              fit: BoxFit.cover,
+            ),
           ),
-        )
-      ],
-    ));
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 35),
+            child: Text(
+              'The best solution for cleaning your clothes',
+              style: TextStyle(
+                fontSize: 35,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(height: 30),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 35),
+            child: Text(
+              'Find the best and closest laundry place to you,cleaned with the best washing machine so it doesn`t reduce the quality of your clothes',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35),
+            child: SafeArea(
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(LoginPage.route());
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 0,
+                    padding: const EdgeInsets.symmetric(vertical: 18),
+                  ),
+                  child: const Text(
+                    'Get started',
+                    style: TextStyle(
+                      color: AppColors.blue,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
